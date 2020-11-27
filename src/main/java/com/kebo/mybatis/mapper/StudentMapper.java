a1a9cc11b10a6d7b0617f8d1cad9856b605c30f6
Administrator <249183617@qq.com>
2020-11-27 16:09:28 +0800

    test

--- src/main/java/com/kebo/mybatis/mapper/StudentMapper.java
+++ src/main/java/com/kebo/mybatis/mapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package com.kebo.mybatis.mapper;
 
 import com.kebo.mybatis.po.Student;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,12 @@ public interface StudentMapper {
     Integer delete(Integer id);
 
     Integer update(Student student);
+
+    List<Student> findByCondition(@Param("name") String name, @Param("sal") Double sal);
+
+    Integer updateByConditions(Student student);
+
+    Integer deleteByConditions(List<Integer> ids);
+
+    Integer insertByConditions(Student student);
 }
--- src/test/java/com/kebo/mybatis/MybatisApplicationTests.java
+++ src/test/java/com/kebo/mybatis/MybatisApplicationTests.java
@@ -1,11 +1,15 @@
 package com.kebo.mybatis;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.kebo.mybatis.mapper.StudentMapper;
 import com.kebo.mybatis.po.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootTest
@@ -34,7 +38,7 @@ class MybatisApplicationTests {
     @Test
     void testFindAll() {
         List<Student> studentList = studentMapper.findAll();
-        for (Student student:studentList) {
+        for (Student student : studentList) {
             System.out.println(student);
         }
     }
@@ -52,6 +56,48 @@ class MybatisApplicationTests {
         student.setName("kebo2");
         student.setSal(22.6);
         Integer number = studentMapper.update(student);
+        System.out.println(number);
+    }
+
+    @Test
+    void testPage() {
+        PageHelper.startPage(1, 2);
+        PageInfo<Student> pageInfo = new PageInfo<>(studentMapper.findAll());
+        System.out.println(pageInfo.getList());
+    }
+
+    @Test
+    void testFindByCondition() {
+        String name = null;
+        Double sal = 26.0;
+        List<Student> byCondition = studentMapper.findByCondition(name, sal);
+        for (Student stu : byCondition) {
+            System.out.println(stu);
+        }
+    }
+
+    @Test
+    void testUpdateByConditions() {
+        Student student = new Student();
+        student.setId(2);
+        student.setSal(22.60);
+        Integer number = studentMapper.updateByConditions(student);
+        System.out.println(number);
+    }
+
+    @Test
+    void testDeleteByConditions() {
+        List<Integer> integerList = Arrays.asList(1, 2, 3);
+        Integer number = studentMapper.deleteByConditions(integerList);
+        System.out.println(number);
+    }
+
+    @Test
+    void testInsertByConditions() {
+        Student student = new Student();
+        student.setId(2);
+        student.setSal(22.60);
+        Integer number = studentMapper.insertByConditions(student);
         System.out.println(number);
     }
 
